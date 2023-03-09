40487e329489e9d7b96c99cdc51d8b041966dcc6
Lucas Cardoso <lucas.emanuel.cardoso@hotmail.com>
2023-03-09 11:11:34 -0300

    Finalizando Aula 08

--- 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,7 +33,7 @@ public class PersonController {
 	
 	@GetMapping(value = "/{id}",
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") String id) {
+	public Person findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
 	
@@ -51,7 +52,8 @@ public class PersonController {
 	} 
 
 	@DeleteMapping(value = "/{id}")
-	public void delete(@PathVariable(value = "id") String id) {
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
